e85f36455d4a8074e1dfaee763feed020ece7be7
Fabrizio <gaetano.fabrizio.fonte-cic@ibm.com>
2022-06-08 13:35:52 +0200

    commit test

---
+++ ProjectGit/src/elenco/classi/Persona.java
@@ -0,0 +1,16 @@
+package elenco.classi;
+
+public class Persona {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
